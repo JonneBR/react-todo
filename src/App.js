212ba9626e2fd66215adf40a5f069ec0ecf9699b
jonnebr <joaomgoncalvesbr@gmail.com>
2021-01-08 13:31:50 -0300

    form css style

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Form from './components/Form'
 
 function App() {
   return (
@@ -6,6 +7,7 @@ function App() {
       <header>
         <h1>My Todo List</h1>
       </header>
+      <Form/>
     </div>
   );
 }
