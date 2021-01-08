e13183e991cc9b0eeb8ac7bcd3ad389176722e81
jonnebr <joaomgoncalvesbr@gmail.com>
2021-01-08 13:08:52 -0300

    header css style

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <header>
+        <h1>My Todo List</h1>
+      </header>
     </div>
   );
 }
